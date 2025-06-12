ded074c9e17622f1d5a00a6fea60d22d76b315d5
Marco Lopez <marco.lopez@corp.zscaler.com>
2025-06-11 21:43:30 -0600

    changes

--- Semana 11/Ejercicio_2_semana_11_lmarcolb89.py
+++ Semana 11/Ejercicio_2_semana_11_lmarcolb89.py
@@ -4,39 +4,57 @@
 #    3. Un método para bajar pasajeros uno por uno (en cualquier orden).
 
 
+
+
+
+
+class Person:
+    def __init__(self, name=""):
+        self.name = name
+
 class Bus:
     def __init__(self, max_passengers):
         self.max_passengers = max_passengers
         self.current_passengers = 0
     
-    def add_passenger(self):
+    def add_passenger(self, person):
+        if not isinstance(person, Person):
+            raise TypeError("Only Person objects can be added as passengers")
+            
         if self.current_passengers < self.max_passengers:
             self.current_passengers += 1
-            print(f"Passenger added. Current passengers: {self.current_passengers}/{self.max_passengers}")
+            print(f"Passenger added (Name: {person.name}). Current: {self.current_passengers}/{self.max_passengers}")
         else:
             print("The bus is currently full")
     
     def drop_passenger(self):
-        """Remove one passenger from the bus if there are any"""
         if self.current_passengers > 0:
             self.current_passengers -= 1
-            print(f"Passenger dropped. Current passengers: {self.current_passengers}/{self.max_passengers}")
+            print(f"Passenger dropped. Current: {self.current_passengers}/{self.max_passengers}")
+        else:
+            print("Bus is empty! No passengers to drop.")
     
     def __str__(self):
         return f"Bus with {self.current_passengers}/{self.max_passengers} passengers"
 
 
-my_bus = Bus(20)
+p1 = Person("Andres")
+p2 = Person("Juan")
+p3 = Person("Manuel")
+p4 = Person("Andrea")
+p5 = Person("Laura")
+p6 = Person("Jose")
+p7 = Person("Karina")
+
+bus = Bus(20)
 
 
-my_bus.add_passenger() 
-my_bus.add_passenger()
-my_bus.add_passenger()
-my_bus.add_passenger()
-my_bus.add_passenger()
-my_bus.add_passenger()
+bus.add_passenger(p1)  
+bus.add_passenger(p2) 
+bus.add_passenger(p3)
+bus.add_passenger(p4)
+bus.add_passenger(p5)
+bus.add_passenger(p6)
+bus.add_passenger(p7)
 
-my_bus.drop_passenger() 
-my_bus.drop_passenger() 
-
-print(my_bus) 
+bus.drop_passenger() 
